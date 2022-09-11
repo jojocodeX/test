6225a056a4125b44f653ff254bb6df05527df064
jojocodex <571943037@qq.com>
2022-09-11 21:27:03 +0800

    test1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,4 +3,8 @@
  * @version @Id: Test.java, v 0.1 2022年09月11日 21:24 jojocodex Exp $
  */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("-====");
+    }
 }
